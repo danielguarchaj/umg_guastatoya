19f101a0af60ed0ba0465a11746e3bbe7d584ee6
DanielGuarchaj <gtdanniel@gmail.com>
2020-07-19 16:36:50 -0600

    initial commit

---
+++ publicaciones/views.py
@@ -0,0 +1,31 @@
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+
+from publicaciones.serializers import (
+    ClasificacionSerializer, 
+    PublicacionSerializer, 
+    PublicacionDepthSerializer,
+)
+from publicaciones.models import (
+    Clasificacion,
+    Publicacion
+)
+
+
+class ClasificacionViewSet(ModelViewSet):
+    serializer_class = ClasificacionSerializer
+    queryset = Clasificacion.objects.all()
+    permission_classes = (IsAuthenticated, )
+
+
+class PublicacionViewSet(ModelViewSet):
+    def get_queryset(self):
+        if 'clasificacion_id' in self.request.query_params:
+            return Publicacion.objects.filter(clasificacion__pk=self.request.query_params['clasificacion_id']).order_by('-fecha_hora_creacion')
+        return Publicacion.objects.all().order_by('-fecha_hora_creacion')
+    
+    def get_serializer_class(self):
+        if self.action == 'list' or self.action == 'retrieve':
+            return PublicacionDepthSerializer
+        return PublicacionSerializer
+
